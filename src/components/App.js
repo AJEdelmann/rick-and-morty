5bbc80e5724baf21df0be88e644eb8c29ef42fb9
ajedelmann <albert.edelmann@gmail.com>
2020-01-27 13:06:40 +0100

    added navigation

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../scss/App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import EpisodesList from "./EpisodesList";
+import CharactersList from "./CharactersList";
+import Navigation from "./Navigation";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/episodes">
+            <EpisodesList />
+          </Route>
+          <Route path="/characters">
+            <CharactersList />
+          </Route>
+          <Route path="/">
+            <Navigation></Navigation>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
